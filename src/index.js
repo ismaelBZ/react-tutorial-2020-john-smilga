846d5e860cdc3268babfd671b575727655283af4
Ismael <ismaelbetzoletti@gmail.com>
2024-02-22 11:23:35 -0300

    ReactDOM.render

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Greeting() {
+  return <h1>Hello World!</h1>;
+}
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
+
