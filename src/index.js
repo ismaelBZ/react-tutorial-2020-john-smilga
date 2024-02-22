cc31eaaf4e8e91a36f5e5972d6ac796257a7b979
Ismael <ismaelbetzoletti@gmail.com>
2024-02-22 11:40:03 -0300

    Arow function 'hoisting' and no brackets

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function Greeting() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Person />
+      <Message />
+    </>
+  );
 }
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+/* Look, dont have brackets, really clean code */
 
+const Person = () => <h1>john doe</h1>;
+const Message = () => <p>This is my message</p>;
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
