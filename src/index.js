70549009d66caa450530fb22dfeb6dbdef7ac410
Ismael <ismaelbetzoletti@gmail.com>
2024-02-22 18:30:40 -0300

    rendering lists - ERROR: Objects aren't valid React child

--- src/index.js
+++ src/index.js
@@ -12,38 +12,34 @@ const Title = styled.h1`
 
 const Image = styled.img``;
 
-const firstBook = {
-  imageURL: "https://m.media-amazon.com/images/I/61p8sBPV5LL._SY342_.jpg",
-  title: "A tartaruguinha",
-  author: "Tia Cris"
-}
+const books = [
+  {
+    imageURL: "https://m.media-amazon.com/images/I/61p8sBPV5LL._SY342_.jpg",
+    title: "A tartaruguinha",
+    author: "Tia Cris",
+  },
 
-const secondBook = {
-  imageURL: "https://m.media-amazon.com/images/I/418M2tXcVjL.AC_SX250.jpg",
-  title: "Wish: o poder dos desejos",
-  author: "Tea Orsi" 
-}
+  {
+    imageURL: "https://m.media-amazon.com/images/I/418M2tXcVjL.AC_SX250.jpg",
+    title: "Wish: o poder dos desejos",
+    author: "Tea Orsi",
+  }
+]
 
-
-const Book = ({image, author, title, children}) => {
+const Book = ({ image, author, title }) => {
   return (
     <article>
       <Image src={image} />
       <Title>{title}</Title>
       <Author>{author}</Author>
-      {children}
     </article>
   );
 };
 
-
 function BookList() {
   return (
     <>
-      <Book image={firstBook.imageURL} author={firstBook.author} title={firstBook.title}>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quae aperiam nulla velit alias excepturi veritatis placeat suscipit animi minus!</p>      
-      </Book>
-      <Book image={secondBook.imageURL} author={secondBook.author} title={secondBook.title} />
+      <div>{books}</div>
     </>
   );
 }
