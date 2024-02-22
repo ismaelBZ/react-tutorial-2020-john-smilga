0a6c24d80e93b82fe39d2238742704db0a48f380
Ismael <ismaelbetzoletti@gmail.com>
2024-02-22 17:30:59 -0300

    studying props

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import styled from "styled-components";
-import data from "./data.json";
 
-function BookList() {
-  return (
-    <>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </>
-  );
-}
 
 const Author = styled.h2`
   font-size: 1.1rem;
 `;
-
 
 const Title = styled.h1`
   font-size: 1.4rem;
@@ -27,18 +13,28 @@ const Title = styled.h1`
 
 const Image = styled.img``;
 
-const title = 'Wish: O Poder Dos Desejos'
-const author = 'Tea Orsi, Dante Luiz'
-const image = 'https://m.media-amazon.com/images/I/418M2tXcVjL.AC_SX250.jpg'
+const title = "Wish: O Poder Dos Desejos";
+const author = "Tea Orsi, Dante Luiz";
+const image = "https://m.media-amazon.com/images/I/418M2tXcVjL.AC_SX250.jpg";
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props)
   return (
     <article>
-      <Image src={image}/>
+      <Image src={image} />
       <Title>{title}</Title>
       <Author>{author}</Author>
     </article>
   );
 };
+
+function BookList() {
+  return (
+    <>
+      <Book job='developer' name='Ismael' />
+      <Book />
+    </>
+  );
+}
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
