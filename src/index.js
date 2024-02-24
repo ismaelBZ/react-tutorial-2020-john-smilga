4aa6307df9cb41bd6ccca45c0385f9717bec50c8
Ismael <ismaelbetzoletti@gmail.com>
2024-02-24 09:20:02 -0300

    Project Blank - hook branch initialized and clear

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { data } from "./data";
-import { Book } from "./Book";
 
-function BookList() {
+function App() {
   return (
     <>
-      {data.map((book) => {
-        return <Book key={book.id} {...book} />;
-      })}
     </>
   );
 }
 
-ReactDOM.render(<BookList />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
