b44038b7ebc966c98a31f190d53d1b077c8047cf
Ismael <ismaelbetzoletti@gmail.com>
2024-02-24 15:02:26 -0300

    useState error-example

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ErrorExample from './tutorials/useState/1-error-example'
+
+const App = () => {
+  return (
+    <div className='container'>
+       <ErrorExample />
+    </div>
+  )
+}
+
+export default App
---
+++ src/tutorials/useState/1-error-example.jsx
@@ -0,0 +1,7 @@
+export default function ErrorExample() {
+  return (
+    <>
+      <h1>Running</h1>
+    </>
+  );
+}
