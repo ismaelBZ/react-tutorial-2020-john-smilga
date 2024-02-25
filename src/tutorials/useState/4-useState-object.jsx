1ae46d2edd238652856d08c0f67dfd0d327264c8
Ismael <ismaelbetzoletti@gmail.com>
2024-02-25 10:57:56 -0300

    Error when change data type/structure in use state

--- src/tutorials/useState/4-useState-object.jsx
+++ src/tutorials/useState/4-useState-object.jsx
@@ -7,10 +7,18 @@ const UseStateObject = () => {
     age: 24,
     message: "random message",
   });
+  const changePerson = () => {
+    setPeople('Peter')
+  }
   console.log(people);
   return (
     <>
      <h1>{people.name}</h1>
+     <h2>{people.age}</h2>
+     <h2>{people.message}</h2>
+     <button className='btn' onClick={changePerson}>
+      Change Person
+     </button>
     </>
   );
 };
