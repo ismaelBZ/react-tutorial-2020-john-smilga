8e493d6f5a2b049186f40c8572d28d17243fad06
Ismael <ismaelbetzoletti@gmail.com>
2024-02-25 12:08:45 -0300

    Error on complex counter - Async function computing with same value

--- src/tutorials/useState/5-useState-counter.jsx
+++ src/tutorials/useState/5-useState-counter.jsx
@@ -3,14 +3,39 @@ import { useState } from "react";
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
 
+
+  const complexIncrease = () => {
+    console.log(value + 1);
+    /* When we click multiple times, the value is assigned on click and stored in register 
+    for a fast computing, so the function will be executed the clicked ammount times with the
+    same value in an interval 
+     */
+    setTimeout(() => {
+      setValue(value + 1);
+    }, 1000 )
+  }
+
   return (
     <>
       <section style={{ margin: "4rem 0" }}>
         <h2>Regular Counter</h2>
-        <h1>{value}</h1>
-        <button className="btn" onClick={() =>setValue(value - 1)}>Decrease</button>
-        <button className="btn" onClick={() => setValue(0)}>Restet</button>
-        <button className="btn" onClick={() => setValue(value + 1)}>Increase</button>
+        <h3 style={{fontSize: "2rem"}}>{value}</h3>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          Decrease
+        </button>
+        <button className="btn" onClick={() => setValue(0)}>
+          Restet
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          Increase
+        </button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Complex Counter</h2>
+        <h3 style={{fontSize: "2rem"}}>{value}</h3>
+        <button className="btn" onClick={complexIncrease}>
+          Increase Later
+        </button>
       </section>
     </>
   );
