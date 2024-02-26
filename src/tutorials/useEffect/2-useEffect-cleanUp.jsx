7704bf1695d2e53c0fd3d3c1ffb4a2e88e5912da
Ismael <ismaelbetzoletti@gmail.com>
2024-02-26 10:52:46 -0300

    refactoring useEffect to run only in componente initial render

--- src/tutorials/useEffect/2-useEffect-cleanUp.jsx
+++ src/tutorials/useEffect/2-useEffect-cleanUp.jsx
@@ -8,20 +8,10 @@ const UseEffectCleanUp = () => {
   }
   
   useEffect(() => {
-   // 2 Second Step -> run useEffect
-   // END -> run the use efect again
-    console.log('use effect')
     window.addEventListener('resize', checkSize);
-    return () => {
-      // 4 Fourth Step -> Befor run useEffect again 
-      // execute the clen up function/useEffect.return
-      console.log('cleanup');
-      window.removeEventListener('resize', checkSize)
-    }
-  })
+  }, [])
   // START -> Render Ccomponent
   // 3 Tirth Step -> Re-render 
-  console.log('Render');
   return (
     <>
       <h2>Window Size</h2>
