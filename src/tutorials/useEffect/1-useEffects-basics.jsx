3faa37380191d832ed014ae46bf1d3c5d1757956
Ismael <ismaelbetzoletti@gmail.com>
2024-02-26 06:27:11 -0300

    useEffect only in initial render

--- src/tutorials/useEffect/1-useEffects-basics.jsx
+++ src/tutorials/useEffect/1-useEffects-basics.jsx
@@ -1,12 +1,21 @@
-import {useEffect} from 'react';
+import { useEffect, useState } from 'react';
 
 export default function UseEffectBasics() {
+  const [value, setValue] = useState(0);
+
+  // By default runs after every re-render
+  // cleanup function
+  // second parameter
   useEffect(() => {
     console.log('call useEffect')
-  });
+  }, []);
   console.log('render component');
   return(
     <>
+      <h2>
+        {value}
+      </h2>
+      <button className="btn" onClick={() => setValue(value + 1)}>Increase</button>
     </>
   );
 }
