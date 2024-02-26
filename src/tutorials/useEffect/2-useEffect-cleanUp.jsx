a662c1a648e368022ebef9497a3457b2f8e745c8
Ismael <ismaelbetzoletti@gmail.com>
2024-02-26 07:33:39 -0300

    Error: bounch of eventListeners added -> check in browser Event Listnes tab

--- src/tutorials/useEffect/2-useEffect-cleanUp.jsx
+++ src/tutorials/useEffect/2-useEffect-cleanUp.jsx
@@ -2,6 +2,27 @@ import {useState, useEffect} from 'react'
 
 const UseEffectCleanUp = () => {
   const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  }
+  
+  useEffect(() => {
+    //  DONT'T WORK
+    setSize(window.innerWidth)
+    //  DON'T WORK
+    window.addEventListener('resize', setSize(window.innerWidth));
+    /*  I think that the previous functions dont't work because the useEffect,
+    like useState, storage the values needed in the register before start the
+    function execution. */
+    
+    // WORK
+    /*  Setting the value inside an anonymous functions, that's not stored and need
+        to be called just in time */
+    //window.addEventListener('resize', () => setSize(window.innerWidth));
+    // WORK
+    window.addEventListener('resize', checkSize)
+  })
   console.log(size);
 
   return (
