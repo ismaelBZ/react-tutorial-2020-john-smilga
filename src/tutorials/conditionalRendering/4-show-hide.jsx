747f93c63a8cb6721d11f916b665db9ef4c3a7c0
Ismael <ismaelbetzoletti@gmail.com>
2024-02-27 10:02:35 -0300

    DEPTH ANALYSIS of useEffect cycle and SOLUTION of bunch event listeners added when render components

--- src/tutorials/conditionalRendering/4-show-hide.jsx
+++ src/tutorials/conditionalRendering/4-show-hide.jsx
@@ -1,13 +1,18 @@
 import {useState, useEffect} from 'react'
 
 const Item = () => {
-  const [size, setSize] = useState(window.innerWidth);
+  const [size, setSize] = useState('default value');
   const checkSize = () => {
-    window.addEventListener('resize', setSize(window.innerWidth))
+    setSize(window.innerWidth);
   }
   useEffect(() => {
+    console.log('Run useEffect')
     window.addEventListener('resize', checkSize);
-  }, [])
+    return () => {
+      console.log('Run return')
+      window.removeEventListener('resize', checkSize);
+    }
+  }, []);
   return (
     <>
      <h2>Window size:</h2>
@@ -15,6 +20,8 @@ const Item = () => {
     </>
   )
 }
+
+/* The component is rendered and display the window.innerWidth value from useState('default value') then useEffect is called and added the window event listener to dom, but not change the value, it will just changed when the window is resized and component is displayed on screen. When hide the component, it's destructed the useEffect return function is runned. Showing the component again, it will rerendered and after that useEffect run again */
 
 const ShowHide = () => {  
   const [show, setShow] = useState(false)
