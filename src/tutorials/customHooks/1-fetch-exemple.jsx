a8740fe83ee2e40149d08435f69f0cbd91488b88
Ismael <ismaelbetzoletti@gmail.com>
2024-02-29 15:26:45 -0300

    custom hook useFetch() concluded

--- src/tutorials/customHooks/1-fetch-exemple.jsx
+++ src/tutorials/customHooks/1-fetch-exemple.jsx
@@ -1,26 +1,10 @@
 import {useState, useEffect} from 'react';
+import {useFetch} from './2-customHook-useFecth';
 
 const url = "https://course-api.com/react-store-products";
 
 const FetchExemple = () => {
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true)
-  
-  const getData = async() => {
-    try {
-      const response = await fetch(url);
-      const json = await response.json();
-      setData(json) 
-      setIsLoading(false)
-    } catch (error) {
-      console.log(error.message)
-    }
-  }
-
-  useEffect(() => {
-    getData(url);
-  }, [])
-
+  const {isLoading, data} = useFetch(url);
   return (
     <>
       {isLoading ? 
--- src/tutorials/customHooks/2-customHook-useFecth.jsx
+++ src/tutorials/customHooks/2-customHook-useFecth.jsx
@@ -1,19 +1,21 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
+
+// Custom hooks every has to start with use
 
 const useFetch = (url) => {
   const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = (true);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const getData = async() => {
+  const getData = async () => {
     const response = await fetch(url);
     const data = await response.json();
     setData(data);
     setIsLoading(false);
-  }
+  };
 
   useEffect(() => getData(), []);
-  
-  return {data, isLoading}
-}
 
-export default useFetch
+  return { data, isLoading };
+};
+
+export {useFetch};
